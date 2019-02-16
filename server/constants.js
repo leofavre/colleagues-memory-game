ba83deac96879c55c10c933050430d91b028707b
leofavre <contato@leofavre.com>
2019-02-16 17:07:18 -0200

    Fix env vars

--- server/constants.js
+++ server/constants.js
@@ -5,9 +5,13 @@ dotenv.config();
 
 const { HOST, PORT } = process.env;
 
+const REDIRECT_URI = PORT
+  ? `${HOST}:${PORT}/oauth`
+  : `${HOST}/oauth`;
+
 module.exports = {
   WEBSITE_PATH: path.resolve(__dirname, '../website'),
-  REDIRECT_URI: `${HOST}:${PORT}/oauth`,
+  REDIRECT_URI,
   AUTH_URI: 'https://github.com/login/oauth/authorize',
   TOKEN_URI: 'https://github.com/login/oauth/access_token',
   ...process.env
